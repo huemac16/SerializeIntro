7a0356d11b72beb75d86107ec55cd326cc44075c
huemac16 <huemac16@htl-kaindorf.at>
2018-09-28 11:57:15 +0200

    added new class schueler

---
+++ SerializeIntro/src/Schueler.java
@@ -0,0 +1,30 @@
+
+import java.time.LocalDate;
+
+public class Schueler {
+
+    private String name;
+    private LocalDate birthday;
+
+    public Schueler(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+}
